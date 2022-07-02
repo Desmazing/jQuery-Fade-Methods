648f46105eccc2175381e1eff12abcbaf8ba0af3
Desmazing <dkimodera1@gmail.com>
2022-07-02 23:37:59 +0300

    fade in and fade out methods jQuery

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 $(document).ready(function(){
     $("#fadeout").click(function(){
         $(".fadeout").fadeOut();
+        $(".fadeout").value = "hidden";
     })
 })
 
 $(document).ready(function(){
     $("#fadein").click(function(){
         $(".fadein").fadeIn();
+        if ($("fadeout").value == "hidden"){
+            $(".fadeout").fadeIn(slow);
+        }
     })
 })
