f095ba4135e0e6b2a046d896e55edd3efab2bd5e
Desmazing <dkimodera1@gmail.com>
2022-07-02 23:07:09 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# A further look at jQuery methods
+### This repo features:
+####    fadeIn() method - fades in hidden elements.
+####    fadeOut() method - fades out displayed elements
---
+++ index.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $("#fadeout").click(function(){
+        $(".fadeout").fadeOut();
+    })
+})
+
+$(document).ready(function(){
+    $("#fadein").click(function(){
+        (".fadein").fadeIn();
+    })
+})
